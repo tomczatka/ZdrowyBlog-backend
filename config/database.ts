e745bcd3e3ce11f43006f74fa4efbdd44edb9e3d
Tomasz Krupa <t.krupa@appfinity.pl>
2022-11-29 16:06:37 +0100

    database changes

--- config/database.ts
+++ config/database.ts
@@ -18,16 +18,30 @@ export default ({ env }) => {
   //     },
   //   }
   // }
-  return{
+  // return{
+  //   connection: {
+  //     client: 'postgres',
+  //     connection: {
+  //       host: env('DATABASE_HOST', '127.0.0.1'),
+  //       port: env.int('DATABASE_PORT', 5432),
+  //       database: env('DATABASE_NAME', 'ZdrowyBlog'),
+  //       user: env('DATABASE_USERNAME', 'postgres'),
+  //       password: env('DATABASE_PASSWORD', 'tomek77'),
+  //       ssl: env.bool('DATABASE_SSL', false),
+  //     },
+  //   },
+  // }
+  const config = parse(process.env.DATABASE_URL)
+  return {
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'ZdrowyBlog'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'tomek77'),
-        ssl: env.bool('DATABASE_SSL', false),
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: false,
       },
     },
   }
